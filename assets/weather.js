c3b7b09336be359414db91e7d07a06dd4c3d529b
William Cheausky <williamchenausky@yahoo.com>
2021-01-28 12:49:02 -0500

    update html and js

--- assets/weather.js
+++ assets/weather.js
@@ -1,46 +1,46 @@
-// glocal vriables
-const searchButton = $("#search-button");
-const searchCity = $("#search-city");
-const clearHistory = $("#clear-history");
-const currentCity = $("current-city");
-const temperature = $("temperature");
-const humidity = $("humidity");
-const windSpeed = $("wind-speed");
-const UVIndex = $("uv-index");
-var city = ""
-var sCity = []
+$(document).ready(function () {
+  $("#search-button").on("click", function () {
+    var cityInput = $("#search-city").val();
+    // empties search input
+    $("#search-city").val("");
 
-function find(c){
-    for (var i=0; i<sCity.length; i++){
-        if(c.toUppercase()===sCity[i]){
-            return -1;
-        }
-    }
-    return 1;
-}
-//Sets API key
-var APIkey = "4c83c9f681a7fa3a6c27e2526f343718";
-// Display current and future weather
-function displayWeather(event){
+    currentWeather(cityInput);
+  });
+
+  $(".list-cities").on("click", "li", function () {
+    currentWeather($(this.text()));
+  });
+
+  function makeRow(text) {
+    var liEl = $("<li>").addClass("list-cities-item-action").text(text);
+    $(".list-cities").append(liEl);
+  }
+
+  var APIkey = "&appid=4c83c9f681a7fa3a6c27e2526f343718";
+  // Display current and future weather
+  function displayWeather(event) {
     event.preventDefault();
-    if(searchCity.val().trim()!==""){
-        city=searchCity.val(),trim();
-        currentWeather(city);
+    if (searchCity.val().trim() !== "") {
+      (city = searchCity.val()), trim();
+      currentWeather(city);
     }
-}
-//set up AJAX call
-function currentWeather(city){
+  }
+  //Sets API key
+var APIkey = "&appid=4c83c9f681a7fa3a6c27e2526f343718";
 
-var queryURL = "api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid" + APIkey;
-$.ajax ({
-    url:queryURL,
-    method: "GET"
-}).then(function(response){
+//set up AJAX call
+function currentWeather(cityInput) {
+  var queryURL =
+    "http://api.openweathermap.org/data/2.5/forecast?q=" + cityInput + APIkey;
+  $.ajax({
+    url: queryURL,
+    method: "GET",
+  }).then(function (response) {
+    console.log(response);
+  });
+}
+
 
 });
-}
 
-console.log("URL:" + queryURL)
 
-//convert temp to F
-var tempF = (response.main.temp - 273.15) * 1.80 + 32;
